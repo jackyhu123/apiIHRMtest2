1672361199c16897389d4da9fc3a80c4024a94b2
jackyhu123 <59444406+jackyhu123@users.noreply.github.com>
2020-01-08 13:17:31 +0800

    Update run_suit.py

--- run_suit.py
+++ run_suit.py
@@ -12,8 +12,7 @@ suite.addTest(unittest.makeSuite(TestIHRMEmp))
 
 # 生成测试报告路径和名称
 # os.path.dirname(os.path.abspath(__file__))的意思是获取当前执行文件的父级的目录
-report_path =app.BASE_DIR + '/report/IHRMLogin{}.html'.format(
-    time.strftime('%Y%m%d-%H%M%S'))
+report_path =app.BASE_DIR + '/report/IHRMLogin.html'
 # 打开测试报告文件，初始化HTMLTestRunner，执行测试套件，生成测试报告
 with open(report_path, mode='wb') as f:
     runner = HTMLTestRunner(f, verbosity=1, title='IHRM人力资源登录接口测试', description='V1.0.0')
